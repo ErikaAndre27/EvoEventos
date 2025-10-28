ae87c21d916b339f51e4ea91030429ecf743b439
ErikaAndre27 <219741158+ErikaAndre27@users.noreply.github.com>
2025-10-27 22:39:29 -0500

    Creación modelos de entidades asociadas a Usuarios (User, DocumentType, Role, CustomerType, Customer)

---
+++ Codigo/Backend/Models/DocumentType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEvoEventos.Models
+{
+    public class DocumentType
+    {
+
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Codigo/Backend/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+namespace BackEvoEventos.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Names { get; set; }
+        public string Surnames { get; set; }
+        public string Email { get; set; }
+        public Guid IdDocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public Guid IdRole { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DocumentType DocumentType { get; set; }
+        public Role Role {  get; set; }
+    }
+}
