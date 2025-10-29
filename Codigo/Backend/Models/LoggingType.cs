09241c66494434239d43f8e772bb4e6115b93571
ErikaAndre27 <219741158+ErikaAndre27@users.noreply.github.com>
2025-10-28 23:37:03 -0500

    Creación modelos Credential y LoggingType

---
+++ Codigo/Backend/Models/LoggingType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEvoEventos.Models
+{
+    public class LoggingType
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
