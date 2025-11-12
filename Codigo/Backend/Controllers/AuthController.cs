d6de284a20faa0fe37807e14d206288496a1d04a
Sergio Gonzalez <alejogon2011@gmail.com>
2025-11-11 20:05:04 -0500

    Implementación JWT

---
+++ Codigo/Backend/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using BackEvoEventos.Models;
+using BackEvoEventos.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BackEvoEventos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IUserRepository UserRepository, IConfiguration Configuration ) // Solo se necesita User y Password
+        {
+            _userRepository = UserRepository;
+            _configuration = Configuration;
+        }
+
+        public async Task<IActionResult> Login(Login Login)
+        {
+
+        }
+    }
+}
---
+++ Codigo/Backend/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEvoEventos.Models
+{
+    public class Login
+    {
+        [Required] // Son campos requeridos
+        public string UserName { get; set; } = string.Empty!;
+
+        [Required]
+        public string Password { get; set; } = string.Empty!; // No permite datos vacios
+
+    }
+}
