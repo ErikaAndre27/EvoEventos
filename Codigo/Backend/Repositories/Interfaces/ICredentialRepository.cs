df624e4aee1ddd7891a3d6a88c8fd59f132d87b5
ErikaAndre27 <219741158+ErikaAndre27@users.noreply.github.com>
2025-11-16 12:59:17 -0500

    Métodos para Login en Credentials

--- Codigo/Backend/Repositories/Interfaces/ICredentialRepository.cs
+++ Codigo/Backend/Repositories/Interfaces/ICredentialRepository.cs
@@ -7,6 +7,7 @@ namespace BackEvoEventos.Repositories.Interfaces
         Task<Credential> CreateCredential(Credential Credential);
         Task<Credential> GetCredentialByIdentifierAndType(string Identifier,Guid IdLoggingType); 
         Task<Credential> GetCredentialByUserId(Guid IdUser);
+        Task<Credential> GetCredentialByIdentifier(String Identifier);
         Task<bool> UpdateLastLogin(Guid CredentialId);
         Task<bool> DeleteCredential(Guid Id);
         Task<bool> UpdatePassword(Guid credentialId, string NewPassword);
