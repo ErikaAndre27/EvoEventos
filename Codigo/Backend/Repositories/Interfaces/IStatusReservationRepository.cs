4eaa1fde976fbfaa923fb3116d458e660be994f2
Jhanpol <jhanpolpb840@gmail.com>
2025-12-07 17:13:19 -0500

    Creación Interfaces, Implementaciones y Controllers : PaymentMethod StatusTransaction StatusReservation

---
+++ Codigo/Backend/Repositories/Interfaces/IStatusReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using BackEvoEventos.Models;
+
+namespace BackEvoEventos.Repositories.Interfaces
+{
+    public interface IStatusReservationRepository
+    {
+        Task<List<StatusReservation>> GetStatusReservations();
+        Task<StatusReservation> GetStatusReservation(Guid Id);
+        Task<bool> CreateStatusReservation(StatusReservation StatusReservation);
+        Task<bool> UpdateStatusReservation(Guid Id, StatusReservation UpdatedStatusReservation);
+        Task<bool> DeleteStatusReservation(Guid Id);
+    }
+}
